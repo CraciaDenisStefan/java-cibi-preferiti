ed5d823e49d43889146bd2b8065da45d7c4fb1d2
CraciDenisStefan <crdenis13@gmail.com>
2023-11-09 19:14:33 +0100

    finito

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -3,6 +3,11 @@ package org.lessons.java.array;
 public class CibiPreferiti {
 	public static void main(String[] args) {
 		
+		String[] classificaCibi = {"Pizza", "Sushi", "Pasta", "Gelato", "Burger", "Pollo"};
+		
+		System.out.println("Lunghezza della classifica: " + classificaCibi.length);
+		System.out.println("Il cibo top è: " + classificaCibi[0]);
+		System.out.println("Il cibo preferito ma non troppo è: " + classificaCibi[classificaCibi.length - 1]);
 		
 	}
 
